50b581601c8a59ee1e64f6a66cc86df864461818
Саша <mkramnichka57446@gmail.com>
2023-11-14 19:33:06 +0200

    add more file

---
+++ login/login.js
@@ -0,0 +1,28 @@
+let email = document.querySelector(".email")
+
+email.addEventListener("input", function () {
+    let emailValue = email.value;
+
+    if (emailValue.length > 5 && emailValue.includes("@")) {
+        email.classList.add("new");
+        email.classList.remove("delate");
+    } else if (emailValue.length === 0) {
+        email.classList.remove("delate")
+        email.classList.remove("new")
+    } else {
+        email.classList.remove("new")
+        email.classList.add("delate")
+    }
+});
+
+let email2 = document.querySelector(".email")
+
+email2.addEventListener("input", function () {
+    let email2Value = email2.value;
+
+    if (email2Value.querySelector == "@") {
+        email2.classList.add("new")
+        email2.classList.remove("delate")
+    }
+
+});
---
+++ script.js
@@ -0,0 +1,80 @@
+let button = document.querySelector(".button-number")
+let cardNumber = document.querySelector(".card_number")
+
+
+button.addEventListener("click", function () {
+    let newNumber = ""
+
+    for (let i = 0; i < 16; i++) {
+        let randomNumber = Math.floor(Math.random() * 10)
+        newNumber += randomNumber
+        if (i % 4 === 3 && i !== 15) {
+            newNumber += ' ';
+        }
+    }
+    cardNumber.textContent = newNumber
+});
+
+
+let buttonCv = document.querySelector(".button-cv")
+let cardCv = document.querySelector(".card_cv")
+
+
+buttonCv.addEventListener("click", function () {
+    let newCv = ""
+
+    for (let i = 0; i < 3; i++) {
+        let randomNumber = Math.floor(Math.random() * 10)
+        newCv += randomNumber
+        if (i % 4 === 3 && i !== 15) {
+            newCv += ' ';
+        }
+    }
+    cardCv.textContent = newCv
+});
+
+let cardData = document.querySelector(".card_year")
+let newData = document.querySelector(".date")
+
+newData.addEventListener("change", function () {
+    let newCardData = newData.value
+    let inputData = newCardData.split("-")
+
+    if (inputData.length === 3) {
+        let day = inputData[0]
+        let month = inputData[1]
+        let year = inputData[2]
+        cardData.innerHTML = month + "/" + year
+    }
+});
+
+let cardName = document.querySelector(".name_name")
+let newName = document.querySelector(".input-name")
+
+newName.addEventListener("input", function () {
+    let newCardName = newName.value
+    cardName.innerHTML = newCardName
+});
+
+
+
+let cardSurname = document.querySelector(".name_surname")
+let newSurname = document.querySelector(".input-surname")
+
+newSurname.addEventListener("input", function () {
+    let newCardSurname = newSurname.value
+    cardSurname.innerHTML = newCardSurname
+});
+
+
+
+let textAbout = document.querySelector(".text-about")
+let chekModal = document.querySelector(".chek-modal")
+
+textAbout.addEventListener("click", function () {
+    if (chekModal.className === "chek-modal") {
+        chekModal.classList.add("active")
+    } else {
+        chekModal.classList.remove("active")
+    }
+});
