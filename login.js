abb8c9389d442439e693f192bf443b3da04e0445
Саша <mkramnichka57446@gmail.com>
2023-11-21 19:28:56 +0200

    додам фото та інше

--- login.js
+++ login.js
@@ -34,3 +34,35 @@ buttonWin.addEventListener("click", (event) => {
     event.preventDefault()
     window.location.href = "nologin.html";
 });
+
+
+
+
+let password = document.querySelector(".password")
+
+password.addEventListener("input", function () {
+    let passwordValue = password.value;
+
+    if (passwordValue.length > 8) {
+        password.classList.add("new");
+        password.classList.remove("delate");
+    } else if (passwordValue.length === 0) {
+        password.classList.remove("delate")
+        password.classList.remove("new")
+    } else {
+        password.classList.remove("new")
+        password.classList.add("delate")
+    }
+});
+
+let password2 = document.querySelector("password")
+
+password2.addEventListener("input", function () {
+    let password2Value = password2.value;
+
+    if (password2Value.querySelector ) {
+        password2.classList.add("new")
+        password2.classList.remove("delate")
+    }
+
+});
