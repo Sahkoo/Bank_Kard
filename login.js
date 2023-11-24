fbfbb05f958c997d05ad674f1cfea096a0c4ee0a
Саша <mkramnichka57446@gmail.com>
2023-11-24 19:08:09 +0200

    редагування js-коду

--- login.js
+++ login.js
@@ -60,9 +60,52 @@ let password2 = document.querySelector("password")
 password2.addEventListener("input", function () {
     let password2Value = password2.value;
 
-    if (password2Value.querySelector ) {
+    if (password2Value.querySelector) {
         password2.classList.add("new")
         password2.classList.remove("delate")
     }
 
+});
+
+
+
+
+
+
+
+
+let button = document.querySelector(".button")
+
+button.addEventListener("clock", function () {
+    let name = document.querySelector(".name")
+    let email = document.querySelector(".email")
+    let password = document.querySelector(".password")
+    let phone = document.querySelector(".phone")
+
+    let userName = name.value
+    let userEmail = email.value
+    let userPassword = password.value
+    let userPhone = phone.value
+
+
+
+    if (userName.length === 0) {
+        alert("Введіть своє ім'я.")
+        return;
+    }
+    else if (userPassword.length < 7) {
+        alert("Пароль повинен складатись що найменше з 7 символів.")
+        return;
+    }
+    else if (userPhone.length !== 13 || !userPhone.includes("+")) {
+        alert("Номер телефону повинен мати 13 символів та +.")
+        return;
+    }
+    else if (userEmail.includes("@")) {
+        alert("Невірний формат електронної адреси.")
+        return;
+    }
+    else {
+        window.location.href = "./index.html"
+    }
 });
